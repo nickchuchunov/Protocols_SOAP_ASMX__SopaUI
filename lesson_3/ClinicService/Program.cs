e1077d8845937f041f01a4dc56780dd22807fb96
Nikolay <nick.chuchunov@yandex.ru>
2022-08-14 19:47:49 +0300

    завершение повторения задания из занятия

--- lesson_3/ClinicService/Program.cs
+++ lesson_3/ClinicService/Program.cs
@@ -1,3 +1,11 @@
+using ClinicService.Data;
+using ClinicService.Services.Impl;
+using ClinicService.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpLogging;
+using NLog.Web;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
 namespace ClinicService
 {
     public class Program
@@ -6,12 +14,58 @@ namespace ClinicService
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.Listen(IPAddress.Any, 5001, listenOptions =>
+                {
+                    listenOptions.Protocols = HttpProtocols.Http2;
+                });
+            });
+
+
+
+
+
+            builder.Services.AddDbContext<ClinicServiceDbContext>(options => 
+            {
+                options.UseSqlServer(builder.Configuration["DatabaseOptions:ConnectionString"]);
+            });
+
+
+            builder.Services.AddGrpc();
+
+            builder.Services.AddHttpLogging(logging =>
+            {
+                logging.LoggingFields = HttpLoggingFields.All | HttpLoggingFields.RequestQuery;
+                logging.RequestBodyLogLimit = 4096;
+                logging.ResponseBodyLogLimit = 4096;
+                logging.RequestHeaders.Add("Authorization");
+                logging.RequestHeaders.Add("X-Real-IP");
+                logging.RequestHeaders.Add("X-Forwarded-For");
+            });
+
+            builder.Host.ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+
+            }).UseNLog(new NLogAspNetCoreOptions() { RemoveLoggerFactoryFilter = true });
+
+            builder.Services.AddScoped<IPetRepository, PetRepository>();
+            builder.Services.AddScoped<IConsultationRepository, ConsultationRepository>();
+            builder.Services.AddScoped<IClientRepository, ClientRepository>();
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
+
+
+
 
             var app = builder.Build();
 
@@ -22,10 +76,32 @@ namespace ClinicService
                 app.UseSwaggerUI();
             }
 
+
+            app.UseWhen( 
+                ctx => ctx.Request.ContentType != "application/grpc",
+                builder =>
+                {
+                    builder.UseHttpLogging();
+                }
+            );
+
+
+
+
+
             app.UseAuthorization();
 
 
             app.MapControllers();
+
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>  // 2
+            {
+                // Communication with gRPC endpoints must be made through a gRPC client.
+                // To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
+                endpoints.MapGrpcService<ClientService>();
+            });
 
             app.Run();
         }
---
+++ lesson_3/ClinicService/Services/impl/ClientService.cs
@@ -0,0 +1,65 @@
+﻿using ClientServiceProtos;
+using ClinicService.Data;
+using Grpc.Core;
+using static ClientServiceProtos.ClientService;
+
+namespace ClinicService.Services.Impl
+{
+    public class ClientService : ClientServiceBase
+    {
+        #region Serives
+
+        private readonly ClinicServiceDbContext _dbContext;
+        private readonly ILogger<ClientService> _logger;
+
+        #endregion
+
+        #region Constructors
+
+        public ClientService(ClinicServiceDbContext dbContext,
+            ILogger<ClientService> logger)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        #endregion
+
+        public override Task<ClientServiceProtos.CreateClientResponse> CreateClient(ClientServiceProtos.CreateClientRequest request, ServerCallContext context)
+        {
+            var client = new Client
+            {
+                Document = request.Document,
+                Surname = request.Surname,
+                FirstName = request.FirstName,
+                Patronymic = request.Patronymic
+            };
+            _dbContext.Clients.Add(client);
+
+            _dbContext.SaveChanges();
+
+            var response = new CreateClientResponse
+            {
+                ClientId = client.ClientId
+            };
+
+            return Task.FromResult(response);
+        }
+
+        public override Task<GetClientsResponse> GetClients(GetClientsRequest request, ServerCallContext context)
+        {
+            var response = new GetClientsResponse();
+            response.Clients.AddRange(_dbContext.Clients.Select(client => new ClientResponse
+            {
+                ClientId = client.ClientId,
+                Document = client.Document,
+                FirstName = client.FirstName,
+                Patronymic = client.Patronymic,
+                Surname = client.Surname
+            }).ToList());
+
+            return Task.FromResult(response);
+        }
+
+    }
+}
